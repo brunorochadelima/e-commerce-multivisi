2e3b5bca15cb70084ea02fb3cae386b0f83ed6ea
Bruno Rocha de Lima <brunorocha33@live.com>
2022-11-25 17:31:45 -0300

    cálculo frete

--- theme/js/product.js
+++ theme/js/product.js
@@ -24,3 +24,37 @@ function validaSeSelecionouVariante() {
   }
 }
 
+function calcularFrete() {
+  const productId = document.querySelector("#product-shipping");
+  const dataProductId = productId.getAttribute("data-productId");
+  const cep1 = document.querySelector("#cep").value.substr(0, 5);
+  const cep2 = document.querySelector("#cep").value.substr(6);
+  const quant = document.querySelector("#quant").value;
+
+  const shippingResult = document.querySelector(".shipping-result");
+
+  fetch(
+    `/mvc/store/product/shipping/?loja=196157&simular=ok&cep1=${cep1}&cep2=${cep2}&quantidade=${quant}&id_produto=${dataProductId}`,
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => {
+      return response.text();
+    })
+
+    .then((data) => {
+      let parser = new DOMParser();
+      xmlDoc = parser.parseFromString(data, "text/html");
+      // texto = xmlDoc.querySelector(".page-simula-frete");
+      resultadoFrete = xmlDoc.getElementsByTagName("table")[1];
+      shippingResult.appendChild(resultadoFrete);
+    })
+
+    .catch((error) => {
+      console.log(error);
+    });
+
+  // Remove o primeiro filho de shippingResult
+  shippingResult.removeChild(shippingResult.firstElementChild);
+}
