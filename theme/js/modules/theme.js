b699ffc2fe1a5194d097cae407babb732687b2f6
Bruno Rocha de Lima <brunorocha33@live.com>
2022-12-02 10:35:04 -0300

    banner cookies

--- theme/js/modules/theme.js
+++ theme/js/modules/theme.js
@@ -1,2 +1,39 @@
-// Menu Mobile
+function cria_cookie(nome, valor) {
+  document.cookie = nome + "=" + valor + "; path=/";
+  // oculta o banner depois de aceitar os cookies
+  banner.style.display = "none";
+}
 
+function get_cookie(nome_cookie) {
+  // Adiciona o sinal de = na frente do nome do cookie
+  let cname = " " + nome_cookie + "=";
+  // Obtém todos os cookies do documento
+  let cookies = document.cookie;
+  // Verifica se seu cookie existe
+  if (cookies.indexOf(cname) == -1) {
+    return false;
+  }
+  // Remove a parte que não interessa dos cookies
+  cookies = cookies.substring(cookies.indexOf(cname), cookies.length);
+  // Obtém o valor do cookie até o ;
+  if (cookies.indexOf(";") != -1) {
+    cookies = cookies.substring(0, cookies.indexOf(";"));
+  }
+  // Remove o nome do cookie e o sinal de =
+  cookies = cookies.split("=")[1];
+  // Retorna apenas o valor do cookie
+  return decodeURI(cookies);
+}
+
+let cookie = get_cookie("banner_aviso_cookies");
+const banner = document.getElementById("banner_cookies");
+
+if (cookie == "true") {
+  banner.style.display = "none";
+} else {
+  banner.style.display = "block";
+  banner.style.position = "fixed";
+  banner.style.bottom = "10px";
+  banner.style.left = "10px"; 
+  banner.style.right = "10px";   
+}
