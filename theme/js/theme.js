2b2336de3cbab05852711bd9d56d2d6b02f4b9a3
brunorochadelima <brunorocha33@live.com>
2023-01-02 09:43:48 -0300

    corrigindo sliders em branco

--- theme/js/theme.js
+++ theme/js/theme.js
@@ -1,3 +1,96 @@
+// Sliders
+
+var swiper1 = new Swiper(".banner-home", {
+  effect: "slide",
+  preloadImages: false,
+  lazy: {
+    loadPrevNext: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  keyboard: true,
+});
+
+var swiper2 = new Swiper(".categories-bar", {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  breakpoints: {
+    250: {
+      slidesPerView: 3.5,
+    },
+    640: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 6,
+      spaceBetween: 40,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Slider da página product
+
+var swiper3 = new Swiper(".thumbsSlider", {
+  spaceBetween: 10,
+  slidesPerView: 6,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+var swiper4 = new Swiper(".productSlider", {
+  preloadImages: false,
+  initialSlide: 1,
+  lazy: {
+    loadPrevNext: true,
+  },
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper3,
+  },
+});
+
+// Slider Showroom
+
+var swiper5 = new Swiper(".showroomSlider", {
+  preloadImages: false,
+  lazy: {
+    loadPrevNext: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  // mousewheel: true,
+  keyboard: true,
+});
+
+
 // Aviso Cookies
 
 const cookie = get_cookie("banner_aviso_cookies");
@@ -54,95 +147,3 @@ function exitModalOverlay() {
   const modal = document.querySelector(".open_menu_categories");
   modal.classList.remove("modal-overlay");
 }
-
-// Sliders
-
-var swiper = new Swiper(".banner-home", {
-  effect: "slide",  
-  preloadImages: false,
-  lazy: {
-    loadPrevNext: true
-  },
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  keyboard: true,
-});
-
-var swiper = new Swiper(".categories-bar", {
-  slidesPerView: 3,
-  spaceBetween: 20,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  breakpoints: {
-    250: {
-      slidesPerView: 3.5,
-    },
-    640: {
-      slidesPerView: 5,
-      spaceBetween: 20,
-    },
-    768: {
-      slidesPerView: 6,
-      spaceBetween: 40,
-    },
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
-// Slider da página product
-
-var swiper = new Swiper(".thumbsSlider", {
-  spaceBetween: 10,
-  slidesPerView: 6,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-
-var swiper2 = new Swiper(".productSlider", {
-  preloadImages: false,
-  initialSlide: 1,
-  lazy: {
-    loadPrevNext: true
-  },
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
-});
-
-// Slider Showroom
-
-var swiper2 = new Swiper(".showroomSlider", {
-  preloadImages: false,
-  lazy: {
-    loadPrevNext: true
-  },
-   
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  // mousewheel: true,
-  keyboard: true,
-});
