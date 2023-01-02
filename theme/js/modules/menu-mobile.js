ebf527f6356f92dd4cbfe08cc75edb28dea5e444
brunorochadelima <brunorocha33@live.com>
2023-01-02 13:49:29 -0300

    corrigindo reload página ao fechar menu

--- theme/js/modules/menu-mobile.js
+++ theme/js/modules/menu-mobile.js
@@ -2,17 +2,21 @@ const btnHamburger = document.querySelector(".btnHamburger");
 const drawer = document.querySelector(".header-mobile-drawer");
 const btncloseDrawer = document.querySelector(".btncloseDrawer");
 
-btnHamburger.addEventListener("click", openDrawer, {passive: true});
-btncloseDrawer.addEventListener("click", closeDrawer, {passive: true});
-btnHamburger.addEventListener("touchstart", openDrawer, {passive: true});
-btncloseDrawer.addEventListener("touchstart", closeDrawer, {passive: true});
+btnHamburger.addEventListener("click", openDrawer, { passive: true });
+btncloseDrawer.addEventListener("click", closeDrawer, { passive: true });
+btnHamburger.addEventListener("touchstart", openDrawer);
+btncloseDrawer.addEventListener("touchstart", closeDrawer);
 
 function openDrawer(event) {
-  if (event.type === "touchstart") event.preventDefault();
+  if (event.type === "touchstart") {
+    event.preventDefault();
+  }
   drawer.classList.add("active");
 }
 
 function closeDrawer(event) {
-  if (event.type === "touchstart") event.preventDefault();
+  if (event.type === "touchstart") {
+    event.preventDefault();
+  }
   drawer.classList.remove("active");
 }
