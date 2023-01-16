476c13f30739f5244152aa922f9d826b2ed7630c
brunorochadelima <brunorocha33@live.com>
2023-01-16 08:48:47 -0300

    refatorando fun calcular frete

--- theme/js/product.js
+++ theme/js/product.js
@@ -24,16 +24,18 @@ function validaSeSelecionouVariante() {
   }
 }
 
-function calcularFrete() {
-  const productId = document.querySelector("#product-shipping");
-  const dataProductId = productId.getAttribute("data-productId");
-  const cep1 = document.querySelector("#cep1");
-  const cep2 = document.querySelector("#cep2");
-  const quant = document.querySelector("#quant").value;
-  const shippingResult = document.querySelector(".shipping-result");
-  const url = `https://www.multivisi.com.br/mvc/store/product/shipping/?loja=196157&simular=ok&cep1=${cep1.value}&cep2=${cep2.value}&quantidade=${quant}&id_produto=${dataProductId}`;
+function calcularFrete() {  
+  const formCalculaFrete = document.forms.formCalculaFrete;
+  const {cep1, cep2} = formCalculaFrete;
 
+  const formCartAdd = document.forms.cartAdd
+  const {quant} = formCartAdd;
+
+  const dataProductId = document.querySelector("#product-shipping").getAttribute("data-productId");
+  const shippingResult = document.querySelector(".shipping-result");
   shippingResult.textContent = "Carregando fretes...";
+
+  const url = `https://www.multivisi.com.br/mvc/store/product/shipping/?loja=196157&simular=ok&cep1=${cep1.value}&cep2=${cep2.value}&quantidade=${quant.value}&id_produto=${dataProductId}`;
 
   fetch(url, {
     method: "GET",
